b95cf0a03beb1199c3a9643441db414d7d8da849
masagus hariadi arief <adi.arief77@gmail.com>
2019-01-08 23:30:57 +0700

    simple app

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1> I'm Learing React and I Love It</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
