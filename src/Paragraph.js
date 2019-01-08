961e971e3db857e66181ab77209cc5ba4a177eaf
masagus hariadi arief <adi.arief77@gmail.com>
2019-01-08 23:43:57 +0700

    add paragraph component

---
+++ src/Paragraph.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Paragraph extends Component {
+  render() {
+    return <p>This paragraph made by adi with using reach </p>;
+  }
+}
+
+export default Paragraph;
